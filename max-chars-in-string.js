27517ef2b8cb85e63cdc58694c107a6b5a016fae
ManuTeMaia <cath@manutemaia.com>
2022-10-27 14:34:04 +0300

    add the best variant to Find max Characters in String task

--- max-chars-in-string.js
+++ max-chars-in-string.js
@@ -1,11 +1,10 @@
-//Дана строка, нужно найти наибольшее число повторяющихся знаков за O(N)
+//Дана строка, нужно найти наибольшее число повторяющихся знаков
 
 function maxCharsInString(str) {
     if (str.length === 0) return false
     let char = ''
-    let count = 0
-    let co = 0
-
+    let co = co2 = 0
+    let max = 0
     // Плохой вариант за O(N^2)
     // for(let i = 0; i < str.length; i++) {
     //     let currCount = 0
@@ -20,23 +19,43 @@ function maxCharsInString(str) {
     //                      }
     //     }
     // }
-    let set = [...new Set(str)]
 
-    for(let i = 0; i < set.length; i++) {
-        let currCount = 0
-        for(let j = 0; j < str.length; j++) {
-            co++
-            if(set[i] === str[j]) {
-                currCount++
-            }
-            if (count < currCount) {
-                count = currCount
-                char = set[i]
-            }
+    // Средний вариант за O(Nk)
+    // let set = [...new Set(str)]
+    //
+    // for(let i = 0; i < set.length; i++) {
+    //     let currCount = 0
+    //     for(let j = 0; j < str.length; j++) {
+    //         co++
+    //         if(set[i] === str[j]) {
+    //             currCount++
+    //         }
+    //         if (count < currCount) {
+    //             count = currCount
+    //             char = set[i]
+    //         }
+    //     }
+    //
+    // }
+
+    // Лучший вариант за O(1)
+    const map = new Map()
+    for(char of str) {
+        co++
+        if(map.has(char)) {
+            map.set(char, map.get(char) + 1)
+        } else {
+            map.set(char, 1)
         }
-
     }
-    console.log(char, co)
+    for(let key of map.keys()) {
+        co2++
+        if(map.get(key) > max) {
+            max = map.get(key)
+            char = key
+        }
+    }
+    console.log(max, char, co, co2)
     return char
 }
 
