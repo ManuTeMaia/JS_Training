035b4e701e9eec0bb5aafe0ba8cef2820fd043b9
ManuTeMaia <cath@manutemaia.com>
2022-10-28 16:22:11 +0300

    add better method to Find Max task

---
+++ find-max.js
@@ -0,0 +1,28 @@
+//Найти максимальное число в последовательности N(N>0)
+
+function findMax(arr) {
+
+    // Вариант хорош только для небольших значений
+    // let res = arr[0]
+    // for(let i = 1; i < arr.length; i ++) {
+    //     if(arr[i] > res) {
+    //         res = arr[i]
+    //     }
+    // }
+    // return res
+
+    // Лучший вариант для сравнения больших объектов
+    let res = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[res]) {
+            res = i
+        }
+    }
+    console.log(arr[res])
+    return arr[res]
+}
+
+findMax([1,2,3,6,10,9]) //10
+findMax([1,2,3,6,4,9]) //9
+findMax([10,2,3,6,4,9]) //10
+findMax(['aaa','ddddd','fdcf']) //10
